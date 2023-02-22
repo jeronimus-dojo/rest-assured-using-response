1918771376b97c9d7aa8fed008e93f01064fc0e4
jeroen <jeroen.vandijk@tui.com>
2023-02-22 11:42:06 +0000

    Initial commit

---
+++ src/test/java/GetUserTest.java
@@ -0,0 +1,59 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import org.hamcrest.CoreMatchers;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GetUserTest {
+    Response response;
+
+    @BeforeSuite()
+    public void setupTests(){
+        RestAssured.baseURI = "https://reqres.in";
+
+        response = given ()
+                        .contentType(ContentType.JSON)
+                        .when()
+                        .get("/api/users/1")
+                        .then()
+                        .extract().response();
+    }
+
+    @Test
+    public void HTTPStatusCode200() {
+        assertThat("HTTP Status is 200", response.statusCode(), equalTo(HTTP_OK));
+    }
+
+    @Test
+    public void returnedData() {
+        // Verify Payload
+        assertThat("Verifying ID is correct", response.jsonPath().getString("data.id"), equalTo("1"));
+        assertThat("Verifying email is correct", response.jsonPath().getString("data.email"), equalTo("george.bluth@reqres.in"));
+        assertThat("Verifying first_name is correct", response.jsonPath().getString("data.first_name"), equalTo("George"));
+        assertThat("Verifying last_name is correct", response.jsonPath().getString("data.last_name"), equalTo("Bluth"));
+        assertThat("Verifying avatar is correct", response.jsonPath().getString("data.avatar"), equalTo("https://reqres.in/img/faces/1-image.jpg"));
+    }
+
+    @Test
+    public void getUserHappyPathAssertingInThen() {
+        RestAssured.
+                when()
+                    .get("/api/users/1")
+                .then()
+                    .assertThat()
+                        .statusCode(200)
+                        .contentType("application/json; charset=utf-8")
+                        .header("Server", "cloudflare")
+                        .body("data.email", equalTo("george.bluth@reqres.in")) // data.email is using Groovy GPath
+                        .body("size()", CoreMatchers.is(2)); // Number of items in the object
+    }
+
+}
---
+++ src/test/java/GetUsersTest.java
@@ -0,0 +1,63 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GetUsersTest {
+    Response response;
+
+    @BeforeSuite()
+    public void setupTests(){
+        RestAssured.baseURI = "https://reqres.in";
+
+        response = given ()
+                        .contentType(ContentType.JSON)
+                        .queryParam("page", "1")
+                        .queryParam("per_page", "2")
+                        .when()
+                        .get("/api/users")
+                        .then()
+                        .extract().response();
+    }
+
+    @Test
+    public void HTTPStatusCode200() {
+        assertThat("HTTP Status is 200", response.statusCode(), equalTo(HTTP_OK));
+    }
+
+    @Test
+    public void responseHeaders() {
+        assertThat("Response Header Content-Type", response.getHeader("Content-Type"), equalTo("application/json; charset=utf-8"));
+    }
+
+    @Test
+    public void pagingMetaData() {
+        // Verify Payload
+        assertThat("Verifying page", response.jsonPath().getInt("page"), equalTo(1));
+        assertThat("Verifying per_page", response.jsonPath().getInt("per_page"), equalTo(2));
+        assertThat("Verifying total", response.jsonPath().getInt("total"), equalTo(12));
+        assertThat("Verifying total_pages", response.jsonPath().getInt("total_pages"), equalTo(6));
+    }
+
+    @Test
+    public void returnedData() {
+        // Use the jsonPath to populate a List, then use the List methods to assert
+        final List<String> data = response.jsonPath().get("data.email");
+        assertThat("Only two users should be returned", data.size(), equalTo(2));
+
+        assertThat("has emails", data, hasItems("george.bluth@reqres.in", "janet.weaver@reqres.in"));
+        assertThat("Verifying email, first entry", response.jsonPath().getString("data[0].email"), equalTo("george.bluth@reqres.in"));
+        assertThat("Verifying first_name, second entry", response.jsonPath().getString("data[1].first_name"), equalTo("Janet"));
+    }
+}
